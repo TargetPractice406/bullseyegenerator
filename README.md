de7a2df37db60ca825557d8a19327ddf10b278e3
Bradford Knowlton <brad@targetpractice406.com>
2022-02-12 22:37:12 -0700

    prior to eject

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # bullseyegenerator
 REACT Landing Page
+
+## Dev Server
+
+    $ CHOKIDAR_USEPOLLING=true HOST=0.0.0.0 npm start
+
+## Testing
+
+    $ CHOKIDAR_USEPOLLING=true HOST=0.0.0.0 npm test
+
+## References
+
+* https://www.smashingmagazine.com/2015/12/generating-svg-with-react/
+* https://betterprogramming.pub/create-react-app-and-svgs-70970ac715f2
+
+
+* https://pspdfkit.com/blog/2017/how-to-build-free-hand-drawing-using-react/
+* https://codepen.io/philipp-spiess/pen/WpQpGr
+* https://coderrocketfuel.com/article/convert-svg-to-png-using-node-js-and-sharp
+* https://techsparx.com/nodejs/graphics/svg-to-png.html
--- src/index.js
+++ src/index.js
@@ -1,12 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+import Immutable from 'immutable';
+
+class DrawArea extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lines: new Immutable.List(),
+      isDrawing: false
+    };
+
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mouseup", this.handleMouseUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mouseup", this.handleMouseUp);
+  }
+
+  handleMouseDown(mouseEvent) {
+    if (mouseEvent.button !== 0) {
+      return;
+    }
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+
+    this.setState(prevState => ({
+      lines: prevState.lines.push(new Immutable.List([point])),
+      isDrawing: true
+    }));
+  }
+
+  handleMouseMove(mouseEvent) {
+    if (!this.state.isDrawing) {
+      return;
+    }
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+    
+    this.setState(prevState =>  ({
+      lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
+    }));
+  }
+
+  handleMouseUp() {
+    this.setState({ isDrawing: false });
+  }
+
+  relativeCoordinatesForEvent(mouseEvent) {
+    const boundingRect = this.refs.drawArea.getBoundingClientRect();
+    return new Immutable.Map({
+      x: mouseEvent.clientX - boundingRect.left,
+      y: mouseEvent.clientY - boundingRect.top,
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className="drawArea"
+        ref="drawArea"
+        onMouseDown={this.handleMouseDown}
+        onMouseMove={this.handleMouseMove}
+      >
+        <Drawing lines={this.state.lines} />
+      </div>
+    );
+  }
+}
+
+function Drawing({ lines }) {
+  return (
+    <svg className="drawing">
+      {lines.map((line, index) => (
+        <DrawingLine key={index} line={line} />
+      ))}
+    </svg>
+  );
+}
+
+function DrawingLine({ line }) {
+  const pathData = "M " +
+    line
+      .map(p => {
+        return `${p.get('x')} ${p.get('y')}`;
+      })
+      .join(" L ");
+
+  return <path className="path" d={pathData} />;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DrawArea />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +110,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
